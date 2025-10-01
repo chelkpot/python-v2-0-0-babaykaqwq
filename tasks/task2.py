658e510e2c0d595195b4976241c1e706fbafc0ec
student <student@corp.kpiot.ru>
2025-10-01 15:35:05 +0500

    sd

--- tasks/task2.py
+++ tasks/task2.py
@@ -3,8 +3,12 @@
 def solve():
 # Ниже пишите решение задачи
 
-   
-
+    z,x,c=map(int,input("Введите количество карандашей,ручек,маркеров: ").split())
+    karandash=3
+    ruchka=karandash+2
+    marker=ruchka+7
+    vso=(z*karandash)+(x*ruchka)+(c*marker)
+    print(vso)
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
